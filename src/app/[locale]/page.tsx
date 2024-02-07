36068263ccf0f68ccf0f521fcb03e1c54482ca83
hacene <zerrouk.mohammed.hacene@gmail.com>
2024-02-07 22:48:12 +0100

    Update translations in ar.json and en.json

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,27 +8,27 @@ type Props = {
 
 export default function Home({ params: { locale } }: Props) {
   unstable_setRequestLocale(locale);
-  const t = useTranslations("Index");
+  const t = useTranslations();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
+          {t('Index.body.get-started-by-editing-and-nbsp')}
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
-        <p> {t("title")}</p>
+      
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
-            rel="noopener noreferrer"
+            rel={t('Index.body.noopener-noreferrer')}
           >
-            By{" "}
+            {t('Index.body.by')}{" "}
             <Image
               src="/vercel.svg"
-              alt="Vercel Logo"
+              alt={t('Index.body.vercel-logo')}
               className="dark:invert"
               width={100}
               height={24}
@@ -42,7 +42,7 @@ export default function Home({ params: { locale } }: Props) {
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
-          alt="Next.js Logo"
+          alt={t('Index.body.next-js-logo')}
           width={180}
           height={37}
           priority
@@ -54,16 +54,16 @@ export default function Home({ params: { locale } }: Props) {
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
-          rel="noopener noreferrer"
+          rel={t('Index.body.noopener-noreferrer-0')}
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
+            docs{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            {t('body.find-in-depth-information-about-next-js-features-and-api')}
+            {t('Index.body.find-in-depth-information-about-next-js-features-and-api')}
           </p>
         </a>
 
@@ -71,16 +71,16 @@ export default function Home({ params: { locale } }: Props) {
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
-          rel="noopener noreferrer"
+          rel={t('Index.body.noopener-noreferrer-1')}
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
+            {t('Index.body.learn')}{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            {t('Index.body.learn-about-next-js-in-an-interactive-course-with-and-nbsp-quizzes')}
           </p>
         </a>
 
@@ -88,16 +88,16 @@ export default function Home({ params: { locale } }: Props) {
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
-          rel="noopener noreferrer"
+          rel={t('Index.body.noopener-noreferrer-2')}
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
+            {t('Index.body.templates')}{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
+            {t('Index.body.explore-starter-templates-for-next-js')}
           </p>
         </a>
 
@@ -105,16 +105,16 @@ export default function Home({ params: { locale } }: Props) {
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
-          rel="noopener noreferrer"
+          rel={t('Index.body.noopener-noreferrer-3')}
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
+            {t('Index.body.deploy')}{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            {t('Index.body.instantly-deploy-your-next-js-site-to-a-shareable-url-with-vercel')}
           </p>
         </a>
       </div>
