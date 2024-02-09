142b6e131e718aaca40b370122e89c97169477e9
hacene <zerrouk.mohammed.hacene@gmail.com>
2024-02-09 11:19:32 +0100

    Make text direction auto + setup shadcn and install basic component

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Cairo, Inter } from "next/font/google";
 import "./globals.css";
 import { ReactQueryClientProvider } from "./queryClientProvider";
+import useTextDirection from "@/useTextDirection";
 
 const inter = Inter({ subsets: ["latin"] });
 const cairo = Cairo({ subsets: ["latin"] });
@@ -17,14 +18,12 @@ export default function RootLayout({
   children: React.ReactNode;
   params: { locale: string };
 }>) {
-  let direction = locale === "ar" ? "rtl" : "ltr";
   let font = locale === "ar" ? cairo.className : inter.className;
- 
+  const direction = useTextDirection(locale);
   return (
     <ReactQueryClientProvider>
-    
       <html lang={locale} dir={direction} >
-        <body className={`${direction} ${font}`}>{children}</body>
+        <body className={`${font}`}>{children}</body>
       </html>
     </ReactQueryClientProvider>
   );
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -17,7 +17,7 @@ export default function Home({ params: { locale } }: Props) {
           {t('Index.body.get-started-by-editing-and-nbsp')}
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
-      
+      <p>{t('lets-go')}</p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
