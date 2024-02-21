db9c695ae046846432e5768fa5ac5a67176fb97e
taleb-boop <tlbreda8@mail.com>
2024-02-21 13:18:26 +0100

    added json translattions to definitions

--- src/app/[locale]/(app)/definitions/definitionsComponent.tsx
+++ src/app/[locale]/(app)/definitions/definitionsComponent.tsx
@@ -9,26 +9,33 @@ import {
   UsersIcon,
 } from "lucide-react";
 import { Link } from "@/config/navigation";
-
-const cards = [
-  { icon: ShapesIcon, link: "", title: "Initiatives types" },
-  { icon: CheckCircle, link: "", title: "Tasks types" },
-  { icon: HashIcon, link: "", title: "Tasks tags" },
-  { icon: UsersIcon, link: "", title: "Teams" },
-  { icon: MapPinIcon, link: "", title: "country" },
-  { icon: Clock, link: "", title: "Time-zones" },
-];
+import { useTranslations } from "next-intl";
 
 const Definitions = () => {
+  const t = useTranslations("definitions");
+
+  const cards = [
+    {
+      icon: ShapesIcon,
+      link: "/initiatives-types",
+      title: t("initiatives-types"),
+    },
+    { icon: CheckCircle, link: "/tasks-types", title: t("tasks-types") },
+    { icon: HashIcon, link: "/task-tags", title: t("tasks-tags") },
+    { icon: UsersIcon, link: "/teams", title: t("teams") },
+    { icon: MapPinIcon, link: "/country", title: t("country") },
+    { icon: Clock, link: "/time-zones", title: t("time-zones") },
+  ];
+
   return (
-    <div className="grid md:grid-cols-3 gap-5 p-5">
+    <div className="grid md:grid-cols-3 gap-5 p-5 ">
       {cards.map((card, index) => (
         <Link href={card.link} key={index}>
           <div>
             <Card className="bg-white transition-colors duration-500 hover:bg-costum-cream py-5">
-              <CardContent className="flex flex-col items-center justify-center">
+              <CardContent className="flex flex-col items-center justify-center space-y-2  h-full p-0">
                 <div>
-                  <card.icon className="size-16" />
+                  <card.icon className="size-16 " />
                 </div>
                 <p>{card.title} </p>
               </CardContent>
